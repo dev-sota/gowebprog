821df9dca443b8af7815ee9bdc0eaebfc4232143
Satoshi Ota <sota.developer@gmail.com>
2021-01-10 13:48:52 +0900

    Change db to mysql

--- data.go
+++ data.go
@@ -3,7 +3,7 @@ package main
 import (
 	"database/sql"
 
-	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var Db *sql.DB
@@ -11,7 +11,7 @@ var Db *sql.DB
 // connect to the Db
 func init() {
 	var err error
-	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	Db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/gwp")
 	if err != nil {
 		panic(err)
 	}
@@ -20,30 +20,31 @@ func init() {
 // Get a single post
 func retrieve(id int) (post Post, err error) {
 	post = Post{}
-	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	err = Db.QueryRow("select * from posts where id = ?", id).Scan(&post.Id, &post.Content, &post.Author)
 	return
 }
 
 // Create a new post
 func (post *Post) create() (err error) {
-	statement := "insert into posts (content, author) values ($1, $2) returning id"
-	stmt, err := Db.Prepare(statement)
+	stmt, err := Db.Prepare("insert into posts (content, author) values (?, ?)")
 	if err != nil {
 		return
 	}
 	defer stmt.Close()
-	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	res, err := stmt.Exec(post.Content, post.Author)
+	postId, err := res.LastInsertId()
+	post.Id = int(postId)
 	return
 }
 
 // Update a post
 func (post *Post) update() (err error) {
-	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	_, err = Db.Exec("update posts set content = ?, author = ? where id = ?", post.Content, post.Author, post.Id)
 	return
 }
 
 // Delete a post
 func (post *Post) delete() (err error) {
-	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	_, err = Db.Exec("delete from posts where id = ?", post.Id)
 	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dev-sota/gowebprog
 
 go 1.15
 
-require github.com/lib/pq v1.9.0
+require github.com/go-sql-driver/mysql v1.5.0
